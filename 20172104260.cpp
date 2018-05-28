6a5b04d5ad282672655ee9b9a1d536f8bf4355b5
20172104260 <1538986489@qq.com>
2018-05-28 16:49:20 +0800

    Initial commit

---
+++ 20172104260.cpp
@@ -0,0 +1,49 @@
+// ConsoleApplication1.cpp : 定义控制台应用程序的入口点。
+//
+
+#include "stdafx.h"
+#include<iostream>
+using namespace std;
+
+class CFeet
+{
+private:
+	int feet;
+	int inches;
+
+public:
+	void display();
+	void conversion(int a, int b);
+	CFeet operator +(CFeet & ojbk);
+};
+void CFeet::display()
+{
+	cout << feet << " feet " << inches << " inches" << endl;
+}
+
+void CFeet::conversion(int fe, int in)
+{
+	feet = fe + in / 12;
+	inches = in % 12;
+}
+
+CFeet CFeet::operator+(CFeet & ojbk)
+{
+	CFeet add;
+	add.conversion(feet + ojbk.feet, inches + ojbk.inches);
+	return add;
+}
+
+
+int main()
+{
+	CFeet A, B, C;
+	A.conversion(10,8);
+	A.display();
+	B.conversion(12,6);
+	B.display();
+	C = A + B;
+	C.display();
+	return 0;
+}
+
